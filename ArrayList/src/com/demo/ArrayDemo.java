f6c08b04f516acaae79395eff741342612494c63
Nayan <nynraut72@gmail.com>
2022-10-12 17:08:29 +0530

    added Arraylist

---
+++ ArrayList/src/com/demo/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		
+		List<String> list =Arrays.asList("Nayan","Rohan","Sayali","Rohit");
+		 
+		System.out.println(list);
+		
+		
+	}
+
+}
